dcf86238cda54da362231a11dd67804211247239
Reason-Wang <reason-wang@gmail.com>
2025-06-03 07:10:10 +0000

    remove wandb export

--- examples/run_agents/run_react_agent_search.sh
+++ examples/run_agents/run_react_agent_search.sh
@@ -1,4 +1,5 @@
-export VLLM_USE_V1=1
+export WANDB_API_KEY="474a8507f7dafc6a6c4203c0b9bc4a7b2abb4cfe"
+
 # Run in single node
 
 set -x
@@ -11,6 +12,7 @@ address_head=$head_node_ip:$port
 
 # export VLLM_ATTENTION_BACKEND=XFORMERS
 # export GLOO_SOCKET_IFNAME=ens10f0np0
+export USE_VLLM_V1=1
 export HYDRA_FULL_ERROR=1
 # Remove existing Ray cluster
 ray stop
@@ -28,47 +30,49 @@ ray start --head --node-ip-address="$head_node_ip" --port=$port  --num-cpus 192 
 # kl_coef=0.01
 # train_dataset="gsm8k"
 
-model=Qwen/Qwen2.5-3B-Instruct
+model=Qwen/Qwen2.5-7B-Instruct
 lr=5e-7
 length=512
-batch_size=128
+val_batch_size=512
+train_batch_size=128
 num_chains=8
 kl_coef=0.001
-train_dataset="orz_math_57k_train"
+train_dataset="./data/rlhf/qa/train_random_8000.json"
+eval_dataset="./data/rlhf/qa/dev_random_500.json"
+tools="[dense_retrieve,answer]"
+# reward_name="qa_f1_reward"
+reward_name="qa_f1_reward_format"
 # adv_estimator=rloo
-# adv_estimator=reinforce_plus_plus
+adv_estimator=reinforce_plus_plus
 # adv_estimator=remax
-adv_estimator=grpo
+# adv_estimator=grpo
 # adv_estimator=gae
 
-agent_type=code
-tools="[code_interpreter]"
-reward_name="math_reward_format"
 entropy_coeff=0.001
 kl_loss_type=mse
+agent_type=react
 max_steps=4
-agent_backend="async_verl"
-project_name="AgentRL"
+prompt_template="qwen-7b-chat"
 total_training_steps=200
-
+project_name="AgentRL"
 
 python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=$adv_estimator \
-    data.train_files=./data/rlhf/math/${train_dataset}.json \
-    data.val_files=./data/rlhf/math/MATH_500.json \
-    data.train_batch_size=$batch_size \
-    agent.agent_type=$agent_type \
-    agent.tools=$tools \
+    data.train_files=${train_dataset} \
+    data.val_files=${eval_dataset} \
+    agent.num_chains=$num_chains \
+    data.val_batch_size=$val_batch_size \
+    data.train_batch_size=$train_batch_size \
+    agent.use_agent=True \
     agent.model_name_or_path=$model \
     agent.max_steps=${max_steps} \
-    agent.backend=${agent_backend} \
-    agent.reward_name=$reward_name \
-    agent.num_chains=$num_chains \
-    agent.use_agent=True \
+    agent.agent_type=$agent_type \
+    agent.tools=${tools} \
+    agent.reward_name=${reward_name} \
+    actor_rollout_ref.model.path=$model \
     actor_rollout_ref.actor.optim.lr=$lr \
     actor_rollout_ref.model.use_remove_padding=False \
-    actor_rollout_ref.model.path=${model} \
-    actor_rollout_ref.actor.ppo_mini_batch_size=32 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=128 \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=2 \
     actor_rollout_ref.actor.use_kl_loss=True \
     actor_rollout_ref.actor.kl_loss_coef=$kl_coef \
@@ -78,7 +82,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.actor.fsdp_config.param_offload=True \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=True \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=4 \
-    actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.response_length=$length \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.5 \
@@ -90,7 +94,7 @@ python3 -m verl.trainer.main_ppo \
     algorithm.kl_ctrl.kl_coef=$kl_coef \
     trainer.critic_warmup=0 \
     trainer.logger=['console','wandb'] \
-    trainer.project_name=$project_name \
+    trainer.project_name=${project_name} \
     trainer.experiment_name="${model}-${train_dataset}-${lr}-${length}-bs${batch_size}-n${num_chains}-kl${kl_loss_type}${kl_coef}-entropy${entropy_coeff}-${max_steps}steps-${adv_estimator}" \
     trainer.n_gpus_per_node=8 \
     trainer.nnodes=1 \
